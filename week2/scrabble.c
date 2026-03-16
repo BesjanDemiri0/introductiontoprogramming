1b4dbf2e3a61bddfb32ee29cd702ffe4e5b8da63
BesjanDemiri0 <demiri.besjan@ibu.edu.mk>
2026-03-17 00:33:35 +0100

    Update scrabble.c Besjan

--- week2/scrabble.c
+++ week2/scrabble.c
@@ -34,11 +34,18 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner (or "Tie!" if scores are equal)
-    //
-    //   If score1 > score2 → printf("Player 1 wins!\n");
-    //   If score2 > score1 → printf("Player 2 wins!\n");
-    //   If equal           → printf("Tie!\n");
-
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 
 // ---------------------------------------------------------------------------
@@ -63,8 +70,19 @@ int compute_score(string word)
     int score = 0;
 
     // TODO: Loop through each character of word
-    // TODO: Add the correct point value to score
-    // TODO: Return the total score
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        // TODO: Add the correct point value to score
+        if (isupper(word[i]))
+        {
+            score += POINTS[word[i] - 'A'];
+        }
+        else if (islower(word[i]))
+        {
+            score += POINTS[word[i] - 'a'];
+        }
+    }
 
+    // TODO: Return the total score
     return score;
 }
